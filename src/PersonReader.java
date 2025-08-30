2bfe81cc443a684e34120df74de6c3e0bf6e0fa1
scarlaos <scarlaos@mail.uc.edu>
2025-08-30 11:39:07 -0400

    init

--- src/PersonReader.java
+++ src/PersonReader.java
@@ -1,5 +1,70 @@
+import javax.swing.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
 public class PersonReader {
     public static void main(String[] args) {
+        JFileChooser chooser = new JFileChooser();
+        File selectedFile;
+        String rec = "";
+        System.out.println("Please enter the path of the file you would like to read: ");
+        try {
+            // uses a fixed known path:
+            //  Path file = Paths.get("c:\\My Documents\\data.txt");
 
+            // use the toolkit to get the current working directory of the IDE
+            // Not sure if the toolkit is thread safe...
+            File workingDirectory = new File(System.getProperty("user.dir"));
+
+            // Typiacally, we want the user to pick the file so we use a file chooser
+            // kind of ugly code to make the chooser work with NIO.
+            // Because the chooser is part of Swing it should be thread safe.
+            chooser.setCurrentDirectory(workingDirectory);
+            // Using the chooser adds some complexity to the code.
+            // we have to code the complete program within the conditional return of
+            // the filechooser because the user can close it without picking a file
+
+            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                selectedFile = chooser.getSelectedFile();
+                Path file = selectedFile.toPath();
+                // Typical java pattern of inherited classes
+                // we wrap a BufferedWriter around a lower level BufferedOutputStream
+                InputStream in =
+                        new BufferedInputStream(Files.newInputStream(file, CREATE));
+                BufferedReader reader =
+                        new BufferedReader(new InputStreamReader(in));
+
+
+                System.out.printf("%-6s %-12s %-12s %-10s %-4s%n","ID", "Firstname", "Lastname", "Title", "YOB");
+                System.out.println("=================================================");
+                // Finally we can read the file LOL!
+                while (reader.ready()) {
+                    rec = reader.readLine();
+                    // echo to screen
+                    String[] fields = rec.split(",");
+                    System.out.printf("%-6s %-12s %-12s %-9s %-4s%n", fields[0], fields[1], fields[2], fields[3], fields[4]);
+                }
+                reader.close(); // must close the file to seal it and flush buffer
+                System.out.println("\n\nData file read!");
+
+
+            } else  // User closed the chooser without selecting a file
+            {
+                System.out.println("No file selected!!! ... exiting.\nRun the program again and select a file.");
+            }
+
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File not found!!!");
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
